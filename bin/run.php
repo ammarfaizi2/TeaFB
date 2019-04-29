957d51e5a6516d9a34eaad6923039430a0137d4e
Ammar Faizi <ammarfaizi2@gmail.com>
2019-04-29 22:45:04 +0700

    Clean up

--- bin/run.php
+++ bin/run.php
@@ -10,14 +10,30 @@ use TeaFB\Utils\Post\React;
 
 $fb = new TeaFB($email, $password, $cookieFile);
 if ($fb->login() === $fb::LOGIN_OK) {
+	$reactChooser = function ($reacts) {
+		$reacts = [];
+		foreach ($react as $k => $v) {
+			for ($i=0; $i < $v; $i++) { 
+				$reacts[] = $v;
+			}
+		}
+		return $reacts[rand(0, count($reacts) - 1)];
+	};
 	$post = new Post($fb);
 	$profile = new Profile($fb);
-	// foreach($profile->visit("ammarfaizi2")->getReactablePosts() as $storyId) {
-	// 	print json_encode($post->visit($storyId)->getContent())."\n";
-	// }
-
-	$posts = $profile->visit("ammarfaizi2")->getReactablePosts();
-	$post->visit($posts[1])->react(React::WOW);
+	$target = json_decode(file_get_contents(__DIR__."/storage/{$username}/target.json"), true);
+	foreach ($target as $username => $v) {
+		print "Visiting target profile: {$username}...\n";
+		foreach($profile->visit($username)->getReactablePosts() as $storyId) {
+			print "Visiting target's post: {$storyId}...\n";
+			$postInfo = $post->visit($storyId);
+			$react = $reactChooser($v);
+			print "Decided to use {$react} react.\n";
+			print "Reacting {$storyId}...";
+			$postInfo->react($react);
+			print "OK\n";
+		}
+	}
 
 } else {
 	printf("Login failed!\n");
