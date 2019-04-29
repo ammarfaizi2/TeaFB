3df53e663605235914dfdea8840196dcbcd0c04c
Ammar Faizi <ammarfaizi2@gmail.com>
2019-04-29 18:48:53 +0700

    Prepare autoloader

---
+++ src/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!defined("__TEA_FB_AUTOLOADER")):
+
+define("__TEA_FB_AUTOLOADER", true);
+
+/**
+ * @param string $class
+ * @return void
+ */
+function teaFBInternalAutoloader(string $class): void
+{
+	if (file_exists($file = __DIR__."/classes/".str_replace("\\", "/", $class).".php")) {
+		require $file;
+	}
+}
+
+spl_autoload_register("teaFBInternalAutoloader");
+
+endif;
